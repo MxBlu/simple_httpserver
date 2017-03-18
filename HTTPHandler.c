314a262eaa62bb653e2a68f169686a049ebfa4f9
Jumail <mundekkat@hotmail.com>
2017-03-18 23:01:08 +1100

    Added MIME type guessing

--- HTTPHandler.c
+++ HTTPHandler.c
@@ -13,7 +13,7 @@
 #define SEND_SUCCESS 1
 #define SEND_FAILED 0
 
-static int sendHTMLHeader(int sockfd, char *status, int fileSize);
+static int sendHTMLHeader(int sockfd, char *status, char *contentType, int fileSize);
 static void guessType(char *filename, char *typeBuf);
 
 int parseRequest(char *clientBuff, char *request) {
@@ -32,6 +32,13 @@ int parseRequest(char *clientBuff, char *request) {
 
     if(request[reqLen - 1] == '/')
         strcat(request, "index.html");
+    else {
+        char *lastDot = strrchr(request, '.');
+        char *lastSlash = strrchr(request, '/');
+
+        if (lastDot < lastSlash)
+            strcat(request, "/index.html");
+    }
 
     token = strtok(NULL, " ");
     if(!(strcmp(token, "HTTP/1.1") || strcmp(token, "HTTP/1.0")))
@@ -61,14 +68,17 @@ void serveHTML(int sockfd, char *file) {
                 "</html>\r\n"
                 "\r\n", file);
 
-        if (sendHTMLHeader(sockfd, "404 Not Found", strlen(fileBuffer)) <= 0)
+        if (sendHTMLHeader(sockfd, "404 Not Found", "text/html; charset=UTF-8", strlen(fileBuffer)) <= 0)
             return;
 
         send(sockfd, fileBuffer, strlen(fileBuffer), 0);
 
     } else {
+        char mimetype[100];
+        guessType(file, mimetype);
+
         int bytes = fread(fileBuffer, 1, MAX_FILESIZE, fileptr);
-        if (sendHTMLHeader(sockfd, "200 OK", bytes) <= 0)
+        if (sendHTMLHeader(sockfd, "200 OK", mimetype, bytes) <= 0)
             return;
 
         send(sockfd, fileBuffer, bytes, 0);
@@ -78,15 +88,26 @@ void serveHTML(int sockfd, char *file) {
     return;
 }
 
-static int sendHTMLHeader(int sockfd, char *status, int fileSize) {
+static int sendHTMLHeader(int sockfd, char *status, char *contentType, int fileSize) {
     char response[1000];
     snprintf(response, 1000, "HTTP/1.1 %s\r\n"
-                             "Content-Type: text/html; charset=UTF-8\r\n"
-                             "Content-Length: %d\r\n\r\n", status, fileSize);
+                             "Content-Type: %s\r\n"
+                             "Content-Length: %d\r\n\r\n", status, contentType, fileSize);
 
     return send(sockfd, response, strlen(response), 0);
 }
 
 static void guessType(char *filename, char *typeBuf) {
-
+    char *dotPtr = strrchr(filename, '.');
+    if(strcmp(dotPtr, ".html") == 0) {
+        strcpy(typeBuf, "text/html");
+    } else if (strcmp(dotPtr, ".css") == 0) {
+        strcpy(typeBuf, "text/css");
+    } else if (strcmp(dotPtr, ".js") == 0) {
+        strcpy(typeBuf, "application/javascript; charset=utf-8");
+    } else if (strcmp(dotPtr, ".png") == 0) {
+        strcpy(typeBuf, "image/png");
+    } else if (strcmp(dotPtr, ".jpg") == 0) {
+        strcpy(typeBuf, "image/jpeg");
+    }
 }
